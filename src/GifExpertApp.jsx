f074f0cb921c7803c451af7100615ea0fa209e47
mikivas07 <miguelvas0711@gmail.com>
2024-06-13 15:39:04 -0400

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+export const GifExpertApp = () => {
+  return (
+    <>
+      {/* {Titulo} */}
+      <h1>GifExpertApp</h1>
+      <h3>GifExpertApp</h3>
+
+      {/* {Input} */}
+
+      {/* {Listado de Gifs} */}
+      {/* {Gif Item} */}
+    </>
+  );
+};
